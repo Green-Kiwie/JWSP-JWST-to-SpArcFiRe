360ad791c64d9e31bc9635b7433f432d9dfc0e1f
Green-Kiwie <kierann.schong@gmail.com>
2025-03-25 02:12:20 -0700

    fixed random forest code

--- src/train_random_forest.py
+++ src/train_random_forest.py
@@ -4,13 +4,13 @@ import randomforest_training_data as rf
 
 arguments = sys.argv
 
-trees_start = int(arguments[0])
-trees_end = int(arguments[1])
-trees_step = int(arguments[2])
+trees_start = int(arguments[1])
+trees_end = int(arguments[2])
+trees_step = int(arguments[3])
 
-feature_start = int(arguments[3])
-feature_end = int(arguments[4])
-feature_step = int(arguments[5])
+feature_start = int(arguments[4])
+feature_end = int(arguments[5])
+feature_step = int(arguments[6])
 
 for tree in range(trees_start, trees_end, trees_step):
     for feature in range(feature_start, feature_end, feature_step):
