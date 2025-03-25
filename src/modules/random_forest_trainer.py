e266c23a8b4b619b0aee2142c312563f4ef15e52
Green-Kiwie <kierann.schong@gmail.com>
2025-03-25 01:42:42 -0700

    added random forest training code for linux

---
+++ src/modules/random_forest_trainer.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+def read_training_data(filepath: str = "randomforest_training_data/data.csv") -> pd.DataFrame:
+    """reads csv filepath and loads into dataframe object"""
+    training_data = pd.read_csv()
+    return training_data
+
+def get_target_training(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
+    """split dataset into features and y values"""
+    data["P_spiral"] = data["P_CW"] + data["P_ACW"]
+    training_values = data.drop(columns = ["P_spiral", "P_CW", "P_ACW", "Unnamed: 0", "name"])
+    target_values = data["P_spiral"]
+    return training_values, target_values
+
+def transform_data(feature_set: pd.DataFrame) -> pd.DataFrame:
+    """scales featureset with label encoders. Returns a dataframe of scaled featuresets"""
+    categorical_cols = feature_set.select_dtypes(include=["object", "category"]).columns
+    encoded_data = feature_set
+
+    le = LabelEncoder()
+    for col in categorical_cols:
+        encoded_data[col] = le.fit_transform(encoded_data[col])
+
+    return encoded_data
+
+def train_random_forest(encoded_data, target_value, num_tress, num_features) -> str:
+    """trains model and returns string value of training data and testing data accuracy"""
+    X_train, X_test, y_train, y_test = train_test_split(encoded_data, target_value, test_size=0.2, random_state=42)
+
+    rf = RandomForestRegressor(n_estimators=num_tress, random_state=42, max_features=num_features)
+    rf.fit(X_train, y_train)
+    training_accuracy = rf.score(X_train, y_train)
+    testing_accuracy = rf.score(X_test, y_test)
+
+    output_string = f"for {num_tress} trees and {num_features} features, training data accuracy: {training_accuracy:.2f}, testing data accuracy: {testing_accuracy:.2f} \n"
+    return output_string
+
+def read_train(num_tress, num_features) -> str:
+    training_data = read_training_data()
+    feature_set, target_values = get_target_training(training_data)
+    encoded_data = transform_data(feature_set)
+    output = train_random_forest(encoded_data, target_values, num_tress, num_features)
+
+    return output 
--- src/train_random_forest.py
+++ src/train_random_forest.py
@@ -1,37 +1,21 @@
-import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-
 import sys
+import randomforest_training_data as rf
 
-with open('randomforest_training_data/output.txt', 'w') as sys.stdout:
-    training_data = pd.read_csv("randomforest_training_data/data.csv")
-    training_data["P_spiral"] = training_data["P_CW"] + training_data["P_ACW"]
-    training_values = training_data.drop(columns = ["P_spiral", "P_CW", "P_ACW", "Unnamed: 0", "name"])
-    target_value = training_data["P_spiral"]
+arguments = sys.args
+trees_start = arguments[0]
+trees_end = arguments[1]
+trees_step = arguments[2]
 
-    #encode columns
-    categorical_cols = training_values.select_dtypes(include=["object", "category"]).columns
-    print(list(categorical_cols))
+feature_start = arguments[3]
+feature_end = arguments[4]
+feature_step = arguments[5]
 
-    encoded_data = training_values
-    le = LabelEncoder()
-    for col in categorical_cols:
-        encoded_data[col] = le.fit_transform(encoded_data[col])
-
-    #splitting training and testing data
-    X_train, X_test, y_train, y_test = train_test_split(encoded_data, target_value, test_size=0.2, random_state=42)
-    
-
-    rf = RandomForestRegressor(n_estimators=150, random_state=42, max_features=40)
-    rf.fit(X_train, y_train)
+for tree in range(trees_start, trees_end, trees_step):
+    for feature in range(feature_start, feature_end, feature_step):
+        training_output = rf.read_train(tree, feature)
+        with open("random_forest_output.txt", 'w') as file:
+            file.write(training_output)
 
 
-    training_accuracy = rf.score(X_train, y_train)
-    print(f"model accuracy with training data: {training_accuracy:.2f}")
 
-    accuracy = rf.score(X_test, y_test)
-    print(f"Model Accuracy with unseen: {accuracy:.2f}")
 
