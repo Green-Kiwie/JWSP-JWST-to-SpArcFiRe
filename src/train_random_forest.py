396e885c622917b0924244a6bcfc327c88249a70
Green-Kiwie <kierann.schong@gmail.com>
2025-03-25 01:42:42 -0700

    added random forest training code

--- src/train_random_forest.py
+++ src/train_random_forest.py
@@ -1,18 +1,16 @@
 import sys
 import randomforest_training_data as rf
 
-<<<<<<< HEAD
-arguments = sys.args
-=======
-arguments = sys.argv
->>>>>>> 766ed5d (added random forest training code)
-trees_start = arguments[0]
-trees_end = arguments[1]
-trees_step = arguments[2]
 
-feature_start = arguments[3]
-feature_end = arguments[4]
-feature_step = arguments[5]
+arguments = sys.argv
+
+trees_start = int(arguments[0])
+trees_end = int(arguments[1])
+trees_step = int(arguments[2])
+
+feature_start = int(arguments[3])
+feature_end = int(arguments[4])
+feature_step = int(arguments[5])
 
 for tree in range(trees_start, trees_end, trees_step):
     for feature in range(feature_start, feature_end, feature_step):
