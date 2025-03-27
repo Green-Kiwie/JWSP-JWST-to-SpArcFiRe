4f6a9f4af9863c358f57ce96464147bed0aff22e
Green-Kiwie <kierann.schong@gmail.com>
2025-03-27 00:24:55 -0700

    added printing to track progress

--- src/train_random_forest.py
+++ src/train_random_forest.py
@@ -17,7 +17,9 @@ feature_step = int(arguments[6])
 
 for tree in range(trees_start, trees_end, trees_step):
     for feature in range(feature_start, feature_end, feature_step):
+        print(f"starting training for {tree} trees and {feature} features")
         training_output = rf.read_train(tree, feature)
+        print(training_output)
         with open("random_forest_output.txt", 'a') as file:
             file.write(training_output)
 
