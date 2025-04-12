cbe02b28df2a1861b7a34d8f378763b19e9f1d65
Green-Kiwie <kierann.schong@gmail.com>
2025-04-12 14:30:41 -0700

    modified functions to add better testing selection

--- src/modules/random_forest_trainer.py
+++ src/modules/random_forest_trainer.py
@@ -14,24 +14,31 @@ class RandomForestTrainer:
     def full_dataset(self) -> pd.DataFrame:
         """returns the full target values"""
         return self._full_dataset
+    
+    def _get_relevant_training_values(self) -> pd.DataFrame:
+        """returns only columns that are important for training"""
+        return self._full_dataset.drop(columns = ["P_spiral", "P_CW", "P_ACW", "Unnamed: 0", "name", "split"])
 
     def train_x(self) -> pd.DataFrame:
         """returns the training dataset"""
-        return self.full_dataset()[self.full_dataset()["split_type"] == "train"].drop(["P_spiral"])
-    
+        indexes =  self.full_dataset()[self.full_dataset()["split"] == "train"].index
+        return self._transformed_dataset.loc[indexes]
+
     def test_x(self, percentage = 1) -> pd.DataFrame:
         """returns a certain percentage of the testing dataset, default 100%"""
-        dataset = self.full_dataset()[self.full_dataset()["split_type"] == "test"].drop(["P_spiral"])
-        return dataset.sample(frac=percentage, random_state=1)
+        test_indices = self.full_dataset()[self.full_dataset()["split"] == "test"].index
+        sampled_indices = self._full_dataset.loc[test_indices].sample(frac=percentage, random_state=42).index
+        return self._transformed_dataset.loc[sampled_indices]
         
     def train_y(self) -> pd.DataFrame:
         """returns the training dataset"""
-        return self.full_dataset()[self.full_dataset()["split_type"] == "train"]["P_spiral"]
+        return self.full_dataset()[self.full_dataset()["split"] == "train"]["P_spiral"]
     
-    def test_y(self, percentage = 1) -> pd.DataFrame:
-        """returns a certain percentage of the testing dataset, default 100%"""
-        dataset = self.full_dataset()[self.full_dataset()["split_type"] == "test"]["P_spiral"]
-        return dataset.sample(frac=percentage, random_state=1)
+    def test_y(self, percentage=1) -> pd.Series:
+        """returns a certain percentage of the testing targets, aligned with test_x"""
+        test_indices = self.full_dataset()[self.full_dataset()["split"] == "test"].index
+        sampled_indices = self._full_dataset.loc[test_indices].sample(frac=percentage, random_state=42).index
+        return self._full_dataset.loc[sampled_indices, "P_spiral"]
     
     def num_trees(self) -> int:
         """returns the number of trees to train the model"""
@@ -43,54 +50,55 @@ class RandomForestTrainer:
     
     def training_r2(self) -> float:
         """returns the r^2 measure (accuracy measure) of the random forest model on training data"""
-        return self._rf_model.score(self.train_x(), self.train_y)
+        return self._rf_model.score(self.train_x(), self.train_y())
 
     def testing_r2(self) -> float:
         """returns the r^2 measure (accuracy measure) of the random forest model on the testing data"""
-        return self._rf_model.score(self.test_x(), self.test_y)
+        return self._rf_model.score(self.test_x(), self.test_y())
         
     def summary_msg(self) -> str:
         """returns a string of format: 
         'for {num of trees} and {num of features} features, training r^2: {accuracy score}, testing r^2: {accuracy score}'"""
         return f"for {self.num_trees()} trees and {self.num_features()} features, training data accuracy: {self.training_r2():.2f}, testing data accuracy: {self.testing_r2():.2f} \n"
     
-    def __init__(self, num_trees: int, num_features: int, split_test_train_function: Callable[[pd.DataFrame, pd.Series]] = train_test_split, split_test_inputs: dict = {"random_state": 1, "test_size": 0.2}):
+    def __init__(self, num_trees: int, num_features: int, split_test_train_function: Callable = train_test_split, split_test_inputs: dict = {"random_state": 42, "test_size": 0.2}):
         self._num_trees = num_trees
         self._num_features = num_features
-        self._full_dataset = self._load_training_data(split_test_train_function, split_test_inputs) 
+        self._full_dataset = self._load_training_data(split_test_train_function, split_test_inputs)
         self._transformed_dataset, self._data_transformer = self.transform_data()
+
         self._rf_model = self._train_random_forest(self.train_x(), self.train_y(), num_trees, num_features)
+        
+        # predictions = self._rf_model.predict(self._transformed_dataset)
+        # self._full_dataset["P_spiral_predicted"] = predictions
+
+        predictions = self._rf_model.predict(self._transformed_dataset)
+        self._full_dataset["P_spiral_predicted"] = pd.Series(predictions, index=self._transformed_dataset.index)
+        del predictions
 
 
-    def train_random_forest(x_train, y_train, num_tress, num_features) -> RandomForestRegressor:
+    def _train_random_forest(self, x_train, y_train, num_tress, num_features) -> RandomForestRegressor:
         """trains model and returns string value of training data and testing data accuracy"""
         rf = RandomForestRegressor(n_estimators=num_tress, random_state=42, max_features=num_features)
         rf.fit(x_train, y_train)
         return rf
 
-# def read_train(num_tress, num_features) -> str:
-#     training_data = read_training_data()
-#     feature_set, target_values = get_target_training(training_data)
-#     encoded_data = transform_data(feature_set)
-#     output = train_random_forest(encoded_data, target_values, num_tress, num_features)
-
-#     return output 
-
 
     def _load_training_data(self, split_test_train_function: Callable, split_test_train_kwargs: dict, filepath: str = "randomforest_training_data/data.csv") -> pd.DataFrame:
         """reads csv filepath and loads into dataframe object, adds P_spiral value"""
-        training_data = pd.read_csv(filepath)
-        training_data = self._get_target(training_data)
-        self._split_training_testing_data(split_test_train_function, split_test_train_kwargs)
-        return training_data
+        dataset = pd.read_csv(filepath)
+        dataset = self._get_target(dataset)
+        dataset = self._split_training_testing_data(dataset, split_test_train_function, split_test_train_kwargs)
+        return dataset
     
-    def _split_training_testing_data(self, split_function: Callable, split_function_kwargs: dict) -> None:
+    def _split_training_testing_data(self, full_dataset, split_function: Callable, split_function_kwargs: dict) -> pd.DataFrame:
         """splits training and testing data then notates it in the original dataframe"""
-        X_train, X_test, _, _ = split_function(self.full_dataset(), self.target_values(), **split_function_kwargs)
+        X_train, X_test, _, _ = split_function(full_dataset, full_dataset["P_spiral"], **split_function_kwargs)
 
-        self.full_dataset()["split"] = None
-        self.full_dataset().loc[X_train.index, "split"] = "train"
-        self.full_dataset().loc[X_test.index, "split"] = "test"
+        full_dataset["split"] = None
+        full_dataset.loc[X_train.index, "split"] = "train"
+        full_dataset.loc[X_test.index, "split"] = "test"
+        return full_dataset
     
     def transform_data(self) -> tuple[pd.DataFrame, LabelEncoder]:
         """scales featureset with label encoders. Returns a dataframe of scaled featuresets"""
@@ -103,16 +111,66 @@ class RandomForestTrainer:
 
         return encoded_data, le
     
-
     @staticmethod
     def _get_target(training_data: pd.DataFrame) -> pd.Series:
         """adds p_spiral metric to training dataframe"""
         training_data['P_spiral'] = training_data["P_CW"] + training_data["P_ACW"]
         return training_data
     
-    def _get_relevant_training_values(self) -> pd.DataFrame:
-        """returns only columns that are important for training"""
-        return self._full_dataset.drop(columns = ["P_spiral", "P_CW", "P_ACW", "Unnamed: 0", "name", "split"])
+
+#function method is flawed, but good idea to just test first. eventually need to implement selection correctly. now would be the same as normal selection
+def bucket_based_split_test(x_data: pd.DataFrame, y_data: pd.Series, num_buckets = 10, random_state = 42, test_size = 0.2) -> tuple[pd.DataFrame, pd.DataFrame, None, None]:
+    """function that splits data based on buckets"""
+    x_test = pd.DataFrame()
+    x_train = pd.DataFrame()
+    print(f"total items: {len(x_data)}")
+
+    bucket_step = 1/num_buckets
+    smallest_bucket_size = _get_smallest_bucket_size(x_data, bucket_step, num_buckets)
+    num_to_select = int(smallest_bucket_size*(1-test_size))
+
+    for i in range(num_buckets):
+        
+        bucket_min = round(i*bucket_step, 3)
+        bucket_max = round((i+1)*bucket_step, 3)
+        
+        if (i == num_buckets-1):
+            bucket_values = x_data[(x_data["P_spiral"] >= bucket_min) & (x_data["P_spiral"] <= bucket_max)]
+        else:
+            bucket_values = x_data[(x_data["P_spiral"] >= bucket_min) & (x_data["P_spiral"] < bucket_max)]
+        print(f"bucket range: {bucket_min} to {bucket_max}, {len(bucket_values)} items")
+
+        x_bucket_train = bucket_values.sample(n=num_to_select, random_state=random_state)
+        x_bucket_test = bucket_values.drop(index=x_bucket_train.index)
+        
+        x_test = pd.concat([x_test, x_bucket_test], axis=0).copy()
+        x_train = pd.concat([x_train, x_bucket_train], axis=0).copy()
+
+        del x_bucket_test, x_bucket_train, bucket_values
+
+    x_test.sort_index()
+    x_train.sort_index()
+    print(f"x_test size: {len(x_test)}")
+    print(f"x_train size: {len(x_train)}")
+    return x_train, x_test, None, None
+
+def _get_smallest_bucket_size(dataframe: pd.DataFrame, bucket_size: float, num_buckets: int) -> int:
+    """returns the smallest bucket size in the dataframe"""
+    smallest_bucket_size = 99999999999
+    for i in range(num_buckets):
+        
+        bucket_min = round(i*bucket_size, 3)
+        bucket_max = round((i+1)*bucket_size, 3)
+
+        bucket_values = dataframe[(dataframe["P_spiral"] >= bucket_min) & (dataframe["P_spiral"] < bucket_max)]
+        if smallest_bucket_size > len(bucket_values):
+            smallest_bucket_size = len(bucket_values)
+    
+    print(f"smallest bucket size: {smallest_bucket_size}")
+    return smallest_bucket_size
+
+
+
         
 
 
--- src/train_random_forest.py
+++ src/train_random_forest.py
@@ -1,27 +1,39 @@
 import sys
+import traceback
 
 sys.path[0] += ('/modules')
 print(sys.path[0])
 import random_forest_trainer as rf
 
 
-arguments = sys.argv
+try:
+    arguments = sys.argv
 
-trees_start = int(arguments[1])
-trees_end = int(arguments[2])
-trees_step = int(arguments[3])
+    trees_start = int(arguments[1])
+    trees_end = int(arguments[2])
+    trees_step = int(arguments[3])
 
-feature_start = int(arguments[4])
-feature_end = int(arguments[5])
-feature_step = int(arguments[6])
+    feature_start = int(arguments[4])
+    feature_end = int(arguments[5])
+    feature_step = int(arguments[6])
 
-for tree in range(trees_start, trees_end, trees_step):
-    for feature in range(feature_start, feature_end, feature_step):
-        print(f"starting training for {tree} trees and {feature} features")
-        trained_model = rf.RandomForestTrainer(tree, feature)
-        print(trained_model.summary_msg())
-        with open("random_forest_output.txt", 'a') as file:
-            file.write(trained_model.summary_msg())
+    num_buckets_input = int(arguments[7])
+
+    for tree in range(trees_start, trees_end, trees_step):
+        for feature in range(feature_start, feature_end, feature_step):
+            print(f"starting training for {tree} trees and {feature} features")
+            trained_model = rf.RandomForestTrainer(tree, feature, split_test_train_function = rf.bucket_based_split_test, split_test_inputs = {"num_buckets": num_buckets_input})
+            print(trained_model.summary_msg())
+            with open(f"random_forest_output.txt", 'a') as file:
+                file.write(trained_model.summary_msg())
+            
+            print(trained_model.full_dataset().columns)
+            trained_model.full_dataset().to_csv(f"random_forest_output/random_forest_output_{tree}_{feature}.csv")
+
+            del trained_model
+except Exception as e:
+    print(f"error: {e}")
+    traceback.print_exc()
 
 
 
