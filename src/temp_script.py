c1177661fd0622a7f2061a40b9f6e3bd9f5d1170
Green-Kiwie <kierann.schong@gmail.com>
2025-10-27 00:54:45 -0700

    added debugging scripts for model training and inference

--- src/temp_script.py
+++ src/temp_script.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 sys.path[0] += ('/modules')
 import reading_sparcfire as sparcfire
+import random_forest_trainer as rf
 
 pd.set_option("display.max_rows", None)
 pd.set_option("display.max_columns", None)
@@ -18,11 +19,14 @@ try:
     infer_filepath = Path(str(arguments[2]))
 
     test_data = pd.read_csv(infer_filepath)
+    print(test_data.columns)
     for col in test_data.columns:
         print(f"{col}: {test_data[col].dtype}")
 
     # input_data = sparcfire.load_training_data(data_filepath)
     infer_data = sparcfire.load_inference_data(infer_filepath, data_filepath)
+
+    trained_model = rf.RandomForestTrainer(150, 110, filepath = data_filepath, split_test_train_function = rf.bucket_based_split_test, split_test_inputs = {"num_buckets": 1, "random_state": 42, "test_size": 0.2})
 
     # print("Input Data Columns and Types:")
     # for col in input_data.columns:
